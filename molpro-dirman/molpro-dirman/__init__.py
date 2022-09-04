d7738cf78d4656bb57fc9fc6cf5cf418382b91e4
MolarFox <molar@molarfox.io>
2022-09-04 20:12:43 +1000

    Create handler for optional rich print dependency

---
+++ molpro-dirman/molpro-dirman/__init__.py
@@ -0,0 +1,26 @@
+
+import re
+
+from .config import *
+from .system import *
+
+RICH_ENABLED = None
+
+# Try importing rich, overriding the stock print function if able
+try:
+  from rich import print
+  RICH_ENABLED = True
+except ModuleNotFoundError:
+  RICH_ENABLED = False
+
+core_print = print  # Retain the original print function ptr
+
+def print(*args, **kwargs) -> None:
+  "Print with rich, or strip rich formatting and print with vanilla print"
+  if RICH_ENABLED:
+    return core_print(*args, **kwargs)
+  return core_print(  
+    *[re.sub("\[.*?\]","",s) for s in args],  # Strip away rich's formatting
+    **kwargs
+  )
+  
--- molpro-dirman/molpro-dirman/__main__.py
+++ molpro-dirman/molpro-dirman/__main__.py
@@ -17,6 +17,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import typer
+from . import print
 
 app = typer.Typer(invoke_without_command=True)
 
