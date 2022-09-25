9bd93e7661f1f996e51b8577bc4ed723afb73d04
MolarFox <molar@molarfox.io>
2022-09-25 18:40:07 +1000

    Implement safe interstitial print function for rich.print_json

--- molpro-dirman/molpro-dirman/__init__.py
+++ molpro-dirman/molpro-dirman/__init__.py
@@ -1,5 +1,6 @@
 
 import re
+import json
 
 from .config import *
 from .sys_read import *
@@ -9,7 +10,7 @@ RICH_ENABLED = None
 
 # Try importing rich, overriding the stock print function if able
 try:
-  from rich import print
+  import rich
   RICH_ENABLED = True
 except ModuleNotFoundError:
   RICH_ENABLED = False
@@ -19,8 +20,20 @@ core_print = print  # Retain the original print function ptr
 def print(*args, **kwargs) -> None:
   "Print with rich, or strip rich formatting and print with vanilla print"
   if RICH_ENABLED:
-    return core_print(*args, **kwargs)
+    return rich.print(*args, **kwargs)
   return core_print(  
     *[re.sub("\[.*?\]","",s) for s in args],  # Strip away rich's formatting
     **kwargs
   )
+
+
+def print_json(*args, **kwargs) -> None:
+  "Print with rich's fancy json output, or use regular json dumps otherwise"
+  if RICH_ENABLED:
+    return rich.print_json(*args, **kwargs)
+  if "data" in kwargs:
+    print(
+      json.dumps(kwargs["data"], indent=kwargs.get("indent", 2))
+    )
+  else:
+    print(kwargs.get("json", args[0]))
